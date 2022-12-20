41d8fec3fe4a9e36c77ec9ab0e308fe0510fcdc7
Krzysztof Findeisen <kfindeis@uw.edu>
2022-12-19 17:20:25 -0800

    Remove dual collection use from import_calibs.py.
    The original DC2 script had a user-specified chained collection and a
    hardcoded calibration collection that had to be a member of the chained
    collection. This change replaces the system with a single
    user-specified collection that may be chained or calibration (once
    Middlewhere supports queries against calibration collections).
    Identifying the collections that hold validity ranges is now handled
    automatically. The resulting UI is less error prone, and doesn't
    require that the user modify the script as new calib reprocessings
    become available.

--- scripts/import_calibs.py
+++ scripts/import_calibs.py
@@ -22,10 +22,14 @@
 
 """Script for copying calibs appropriate for these exposures.
 
+This script currently requires that the calibration collection be part of a
+chained collection, so that it can query the latter to identify the subset of
+calibs to export. This restriction should go away after DM-37409.
+
 Example:
-$ python import_calibs.py -c "u/me/DM-123456-calib"
-imports image calibrations from u/me/DM-123456-calib in /repo/dc2 to
-calibs in this dataset's preloaded repo.
+$ python import_calibs.py -c "u/me/DM-123456-calib-chain"
+imports image calibrations from (the calibration collections in)
+u/me/DM-123456-calib in /repo/main to calibs in this dataset's preloaded repo.
 """
 
 import argparse
@@ -51,7 +55,7 @@ def _make_parser():
     parser.add_argument("-b", dest="src_dir", default="/repo/dc2",
                         help="Repo to import from, defaults to '/repo/dc2'.")
     parser.add_argument("-c", dest="src_collection", required=True,
-                        help="Calib collection to import from.")
+                        help="Calib collection to import from. Must be a chained collection before DM-37409.")
     return parser
 
 
@@ -66,7 +70,6 @@ DATA_IDS = [dict(detector=164, visit=982985, physical_filter="r_sim_1.4", instru
 CALIB_NAMES = ("bias", "dark", "flat")
 CALIB_COLLECTION = "LSSTCam-imSim/calib"
 DATASET_REPO = os.path.join(os.environ["AP_VERIFY_CI_DC2_DIR"], "preloaded")
-ORIGINAL_CALIB_COLLECTION = "2.2i/calib/gen2"
 
 
 def _export(butler, export_file):
@@ -82,8 +85,8 @@ def _export(butler, export_file):
 
     Returns
     -------
-    runs : iterable [`str`]
-        The names of the runs containing exported templates.
+    calib_collections : iterable [`str`]
+        The names of the calibration collections containing validities.
     """
     with butler.export(filename=export_file, transfer=None) as contents:
         for name in CALIB_NAMES:
@@ -91,11 +94,43 @@ def _export(butler, export_file):
                 calibs = butler.registry.queryDatasets(name, dataId=data_id, collections=butler.collections)
                 contents.saveDatasets(calibs)
 
-        contents.saveCollection(ORIGINAL_CALIB_COLLECTION)
-        # Do not save butler.collections -- if they are RUN collections, it's
-        # redundant; if they are CHAINED, they likely contain content that
-        # isn't being transferred.
-        return {c.run for c in calibs}
+        calib_collections = set()
+        for collection in butler.collections:
+            calib_collections.update(_save_validities(butler.registry, contents, collection))
+        return calib_collections
+
+
+def _save_validities(registry, exporter, collection):
+    """Transfer the validity information found in a collection or any of its
+    sub-collections.
+
+    This function is guaranteed not to add any datasets to the exporter.
+
+    Parameters
+    ----------
+    registry : `lsst.daf.butler.Registry`
+        The registry managing the collections.
+    exporter : `lsst.daf.butler.transfers.RepoExportContext`
+        The export manager to which to copy validities.
+    collection : `str`
+        The collection from which to copy validities.
+
+    Returns
+    -------
+    calib_collections : iterable [`str`]
+        All the individual calibration collections that were exported.
+    """
+    match registry.getCollectionType(collection):
+        case CollectionType.CALIBRATION:
+            exporter.saveCollection(collection)
+            return {collection}
+        case CollectionType.CHAINED:
+            calib_collections = set()
+            for child in registry.getCollectionChain(collection):
+                calib_collections.update(_save_validities(registry, exporter, child))
+            return calib_collections
+        case _:
+            return []
 
 
 def _import(butler, export_file, base_dir):
@@ -115,12 +150,12 @@ def _import(butler, export_file, base_dir):
 
 with tempfile.NamedTemporaryFile(suffix=".yaml") as export_file:
     src = Butler(args.src_dir, collections=args.src_collection, writeable=False)
-    runs = _export(src, export_file.name)
+    calib_collections = _export(src, export_file.name)
     dest = Butler(DATASET_REPO, writeable=True)
     _import(dest, export_file.name, args.src_dir)
     dest.registry.registerCollection(CALIB_COLLECTION, CollectionType.CHAINED)
     chain = list(dest.registry.getCollectionChain(CALIB_COLLECTION))
-    chain.insert(0, ORIGINAL_CALIB_COLLECTION)
+    chain.extend(calib_collections)
     dest.registry.setCollectionChain(CALIB_COLLECTION, chain)
 
 logging.info(f"Calibs stored in {DATASET_REPO}:{CALIB_COLLECTION}.")
